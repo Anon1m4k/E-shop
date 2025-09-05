b03087cc665b1f89c1c790902bae1c492a9dff24
Anon1m4k <kirilvalera2@gmail.com>
2025-09-05 09:29:21 +0300

    Добавили документ описания предметной области

--- MainForm.cs
+++ MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace E_shop
 {
-    public partial class Form1: Form
+    public partial class MainForm: Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
 
