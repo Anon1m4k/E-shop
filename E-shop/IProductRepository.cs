58f3ca8e58bb958cbb5279805f15ad0b8a386ea5
Anon1m4k <kirilvalera2@gmail.com>
2025-09-25 19:42:28 +0300

    Написал тесты на проверку добавления товара

---
+++ E-shop/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace E_shop
+{
+    public interface IProductRepository
+    {
+        void AddProduct(Product product);
+        bool RemoveProduct(Product product);
+        Product GetProductByArticle(string article);
+        List<Product> GetAllProducts();
+        bool ArticleExists(string article);
+    }
+}
