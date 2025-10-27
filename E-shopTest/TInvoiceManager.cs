e7ff91f16ab1dc081da06a178d3b3c576869eae5
Катя <20katerina0060@gmail.com>
2025-10-27 16:33:21 +0300

    Добавила тест для накладной с несколькими позициями

--- E-shopTest/TInvoiceManager.cs
+++ E-shopTest/TInvoiceManager.cs
@@ -127,5 +127,45 @@ namespace E_shopTest
             Assert.AreEqual("Артикул товара не может быть пустым", result);
             mockRepository.Verify(r => r.AddInvoice(It.IsAny<Invoice>()), Times.Never);
         }
+        [TestMethod]
+        public void TestAddInvoiceWithMultipleItems()
+        {
+            var mockRepository = new Mock<IInvoiceRepository>();
+            var manager = new InvoiceManager(mockRepository.Object);
+
+            var validInvoice = new Invoice
+            {
+                ID_Invoice = 5,
+                Date = new DateTime(2025, 10, 26),
+                Items = new List<InvoiceItem>
+            {
+            new InvoiceItem
+            {
+                Article = "22",
+                Name = "Мышка компьютерная",
+                Category = "Техника",
+                Price = 1000,
+                Quantity = 10,
+                Unit = "шт"
+            },
+            new InvoiceItem
+            {
+                Article = "33",
+                Name = "Коврик для мышки",
+                Category = "Аксессуары",
+                Price = 1000,
+                Quantity = 10,
+                Unit = "шт"
+            }
+            }
+            };
+
+            mockRepository.Setup(r => r.AddInvoice(validInvoice)).Returns("Приходная накладная успешно добавлена");
+
+            var result = manager.AddInvoice(validInvoice);
+
+            Assert.AreEqual("Приходная накладная успешно добавлена", result);
+            mockRepository.Verify(r => r.AddInvoice(validInvoice), Times.Once);
+        }
     }
 }
