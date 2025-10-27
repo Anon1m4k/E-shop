0486575f288f174b53bbf0762327abe8b12d3f16
Катя <20katerina0060@gmail.com>
2025-10-27 16:02:22 +0300

    Добавила интерфейс IInvoiceRepository

---
+++ E-shopLib1/IInvoiceRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_shopLib1
+{
+    public interface IInvoiceRepository
+    {
+        string AddInvoice(Invoice invoice);
+    }
+}
