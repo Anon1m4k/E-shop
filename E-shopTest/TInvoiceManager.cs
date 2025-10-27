952a10e9bc56d0b8d081a8c5312280ff4a058fd3
Катя <20katerina0060@gmail.com>
2025-10-27 16:24:45 +0300

    Добавила тест для накладной с отрецательной ценой

--- E-shopTest/TInvoiceManager.cs
+++ E-shopTest/TInvoiceManager.cs
@@ -39,5 +39,35 @@ namespace E_shopTest
             Assert.AreEqual("Приходная накладная успешно добавлена", result);
             mockRepository.Verify(r => r.AddInvoice(validInvoice), Times.Once);
         }
+
+        [TestMethod]
+        public void TestAddInvoiceWithNegativePrice()
+        {
+            var mockRepository = new Mock<IInvoiceRepository>();
+            var manager = new InvoiceManager(mockRepository.Object);
+
+            var invalidInvoice = new Invoice
+            {
+                ID_Invoice = 2,
+                Date = new DateTime(2025, 10, 26),
+                Items = new List<InvoiceItem>
+            {
+            new InvoiceItem
+            {
+                Article = "123",
+                Name = "Компьютер",
+                Category = "Техника",
+                Price = -1000,
+                Quantity = 10,
+                Unit = "шт"
+            }
+            }
+            };
+
+            var result = manager.AddInvoice(invalidInvoice);
+
+            Assert.AreEqual("Цена товара должна быть положительной", result);
+            mockRepository.Verify(r => r.AddInvoice(It.IsAny<Invoice>()), Times.Never);
+        }
     }
 }
