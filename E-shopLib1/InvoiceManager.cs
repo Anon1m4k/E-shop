72e0ec297f471eec4b76ce75ebafad3e131b56a0
Катя <20katerina0060@gmail.com>
2025-10-27 16:08:03 +0300

    Добавила класс InvoiceManager

---
+++ E-shopLib1/InvoiceManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_shopLib1
+{
+    public class InvoiceManager
+    {
+        private IInvoiceRepository repository;
+        public InvoiceManager(IInvoiceRepository repоsitori_)
+        {
+            repository = repоsitori_;
+        }
+        public string AddInvoice(Invoice invoice)
+        {
+
+            return "Все отлично";
+        }
+    }
+}
