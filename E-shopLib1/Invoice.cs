cc74abdab52ee6c6e973573d873a364d12dec78f
Катя <20katerina0060@gmail.com>
2025-10-27 15:57:31 +0300

    Добавила класс  Invoice

---
+++ E-shopLib1/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_shopLib1
+{
+    public class Invoice
+    {
+        public int ID_Invoice { get; set; }
+        public DateTime Date { get; set; }
+        public List<InvoiceItem> Items { get; set; }
+    }
+}
