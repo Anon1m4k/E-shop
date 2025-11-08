d760cadae027fe44dc815e83cff033e62cc88c57
Катя <20katerina0060@gmail.com>
2025-11-08 18:52:33 +0300

    Добавила структуру класса InvoiceForm, конструктор и          InitializeForm.

--- E-shop/InvoiceForm.cs
+++ E-shop/InvoiceForm.cs
@@ -7,14 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using E_shopLib1;
 
 namespace E_shop
 {
     public partial class InvoiceForm: Form
     {
+        private InvoiceManager invoiceManager;
+        private Invoice currentInvoice;
+        private BindingList<InvoiceItem> invoiceItems;
         public InvoiceForm()
         {
             InitializeComponent();
+            InitializeForm();
+        }
+        private void InitializeForm()
+        {
+            invoiceManager = new InvoiceManager(new SQLInvoiceRepository());
+            currentInvoice = invoiceManager.CreateNewInvoice();
+
+            lblDate.Text = currentInvoice.Date.ToString("dd.MM.yyyy");
+
+            invoiceItems = new BindingList<InvoiceItem>();
+            dataGridViewItems.DataSource = invoiceItems;
+
+            dataGridViewItems.CellEndEdit += dataGridViewItems_CellEndEdit;
+
+            UpdateTotalAmount();
         }
     }
 }
