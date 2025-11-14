3440c82c14fa70c4604ef1befd023964d22ee809
Катя <20katerina0060@gmail.com>
2025-11-14 18:25:04 +0300

    Добавила метод GetAllInvoices

--- E-shopLib1/IInvoiceRepository.cs
+++ E-shopLib1/IInvoiceRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace E_shopLib1
+﻿using System.Collections.Generic;
+
+namespace E_shopLib1
 {
     public interface IInvoiceRepository
     {
         string AddInvoice(Invoice invoice);
         int GetNextInvoiceId();
         Invoice GetInvoiceById(int id);
+        List<Invoice> GetAllInvoices();
     }
 }
