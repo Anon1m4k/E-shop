c72c31140a80f1dbffe4cb954b37e9e087ea076d
Anon1m4k <kirilvalera2@gmail.com>
2025-11-18 18:06:40 +0300

    Добавил ещё одну книжечку и изменил тест CategorySelected_WhenCategorySelected_ShouldDisplayCorrectProducts

--- E-shopTest/TCategoryPresenter.cs
+++ E-shopTest/TCategoryPresenter.cs
@@ -3,6 +3,7 @@ using E_shopLib;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace E_shopTest
 {
@@ -33,45 +34,54 @@ namespace E_shopTest
         }
 
         [TestMethod]
-        [DataRow("Одежда", new string[] { "111", "112" }, new string[] { "Футболка", "Джинсы" })]
-        [DataRow("Электроника", new string[] { "1", "2" }, new string[] { "Смартфон", "Ноутбук" })]
-        [DataRow("Книги", new string[] { "111", "222" }, new string[] { "Роман", "Учебник" })]
-        public void CategorySelected_WhenCategorySelected_ShouldDisplayCorrectProducts(string category, string[] expectedArticles, string[] expectedNames)
+        [DataRow("Одежда")]
+        [DataRow("Электроника")]
+        [DataRow("Книги")]
+        public void CategorySelected_WhenCategorySelected_ShouldDisplayCorrectProducts(string category)
         {
             // Arrange
             Mock<ICategoriesView> mockCategoriesView = new Mock<ICategoriesView>();
             Mock<IProductsView> mockProductsView = new Mock<IProductsView>();
             Mock<IProductRepository> mockRepository = new Mock<IProductRepository>();
-            Mock<ProductCatalogManager> mockCatalogManager = new Mock<ProductCatalogManager>(mockRepository.Object);
 
+            // Расширенный список товаров с большим количеством книг
             List<Product> allProducts = new List<Product>
             {
                 new Product { Article = "1", Name = "Смартфон", Category = "Электроника", Price = 15000, Stock = 10, Unit = "шт" },
                 new Product { Article = "2", Name = "Ноутбук", Category = "Электроника", Price = 45000, Stock = 5, Unit = "шт" },
                 new Product { Article = "111", Name = "Футболка", Category = "Одежда", Price = 500, Stock = 15, Unit = "шт" },
                 new Product { Article = "112", Name = "Джинсы", Category = "Одежда", Price = 1500, Stock = 8, Unit = "шт" },
-                new Product { Article = "111", Name = "Роман", Category = "Книги", Price = 400, Stock = 20, Unit = "шт" },
-                new Product { Article = "222", Name = "Учебник", Category = "Книги", Price = 1200, Stock = 35, Unit = "шт" }
+                new Product { Article = "201", Name = "Роман", Category = "Книги", Price = 400, Stock = 20, Unit = "шт" },
+                new Product { Article = "202", Name = "Учебник", Category = "Книги", Price = 1200, Stock = 35, Unit = "шт" },
+                new Product { Article = "203", Name = "Детектив", Category = "Книги", Price = 350, Stock = 12, Unit = "шт" },
             };
 
-            mockCatalogManager.Setup(m => m.GetAllProducts()).Returns(allProducts);
+            mockRepository.Setup(m => m.GetAllProducts()).Returns(allProducts);
 
-            SaleCheckPresenter presenter = new SaleCheckPresenter(mockCategoriesView.Object, mockProductsView.Object, mockRepository.Object);
+            var presenter = new SaleCheckPresenter(mockCategoriesView.Object, mockProductsView.Object, mockRepository.Object);
+
+            // Фильтруем товары по выбранной категории внутри теста
+            var expectedProducts = allProducts.Where(p => p.Category == category).ToList();
 
             // Act
             mockCategoriesView.Raise(v => v.CategorySelected += null, category);
 
-            // Assert
-            mockProductsView.Verify(v => v.DisplayProducts(It.Is<List<Product>>(products =>
-                products.Count == expectedArticles.Length &&
-                CheckProducts(products, expectedArticles, expectedNames))), Times.Once);
+            // Assert - проверяем, что DisplayProducts был вызван с правильным отфильтрованным списком
+            mockProductsView.Verify(v => v.DisplayProducts(It.Is<List<Product>>(actualProducts =>
+                ProductsAreEqual(actualProducts, expectedProducts)
+            )), Times.Once);
         }
 
-        private bool CheckProducts(List<Product> products, string[] expectedArticles, string[] expectedNames)
+        private bool ProductsAreEqual(List<Product> actual, List<Product> expected)
         {
-            for (int i = 0; i < expectedArticles.Length; i++)
+            if (actual.Count != expected.Count)
+                return false;
+
+            for (int i = 0; i < actual.Count; i++)
             {
-                if (products[i].Article != expectedArticles[i] || products[i].Name != expectedNames[i])
+                if (actual[i].Article != expected[i].Article ||
+                    actual[i].Name != expected[i].Name ||
+                    actual[i].Category != expected[i].Category)
                     return false;
             }
             return true;
