ec428660b8b19ef577b5aca7e303d7ede6e0774f
Катя <20katerina0060@gmail.com>
2025-11-24 19:05:32 +0300

    Добавила тест печати накладной с двумя товарами

--- E-shopTest/TPrintableInvoiceManager.cs
+++ E-shopTest/TPrintableInvoiceManager.cs
@@ -39,5 +39,40 @@ namespace E_shopTest
                 string expectedHtml = File.ReadAllText(@"C:\Смирнова\1.html", Encoding.UTF8);
                 Assert.AreEqual(NormalizeHtml(expectedHtml), NormalizeHtml(actualHtml));
             }
+            [TestMethod]
+            public void Test_MultipleItemsInvoice_ShouldMatchExpectedHtml()
+            {
+                var invoice = new Invoice
+                {
+                    SerialNumber = "ABCD425_123*",
+                    Date = new DateTime(2025, 11, 11),
+                    Items = new List<Product>
+            {
+                new Product
+                {
+                    Article = "123",
+                    Name = "Молоток отечественный",
+                    Category = "Молотки",
+                    Stock = 10,
+                    Price = 100m,
+                    Unit = "шт."
+                },
+                new Product
+                {
+                    Article = "BE425 0",
+                    Name = "Топор лесной",
+                    Category = "Топоры",
+                    Stock = 2000,
+                    Price = 500m,
+                    Unit = "шт."
+                }
+            }
+                };
+
+                string actualHtml = _creator.GenerateInvoiceHtml(invoice);
+                string expectedHtml = File.ReadAllText(@"C:\Смирнова\2.html", Encoding.UTF8);
+                Assert.AreEqual(NormalizeHtml(expectedHtml), NormalizeHtml(actualHtml));
+            }
         }
+    }
 }
